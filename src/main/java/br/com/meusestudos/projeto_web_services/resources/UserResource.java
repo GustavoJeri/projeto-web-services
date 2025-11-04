49f5bc1ee87f3ca3a7f35cb35ccbba87cceef935
GustavoJeri <gustavo1italo@gmail.com>
2025-11-03 21:08:13 -0300

    User entity and resource

---
+++ src/main/java/br/com/meusestudos/projeto_web_services/resources/UserResource.java
@@ -0,0 +1,18 @@
+package br.com.meusestudos.projeto_web_services.resources;
+
+import br.com.meusestudos.projeto_web_services.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource{
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Maria", "Maria@gmail.com", "8899999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+}
